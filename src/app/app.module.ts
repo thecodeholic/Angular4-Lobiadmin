0e4493813fd6ca95eccf64628a5f6bcc6f14f030
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2017-03-31 18:09:03 +0400

    Generate navigation, toolbar components
    Make toolbar component styles working
    Install ng2-bootstrap, bootstrap 3.3.7 and font-awesome
    Must run `npm install`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
