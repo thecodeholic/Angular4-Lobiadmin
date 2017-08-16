b8fc619cda2632457e2eb79063f617872b8e0a47
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2017-08-16 16:25:07 +0400

    Enable tooltips

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {MainComponent} from './main.component';
 import {NavigationComponent} from '../core/components/navigation/navigation.component';
 import {ToolbarComponent} from '../toolbar/toolbar.component';
 import {MainRoutingModule} from './main-routing.module';
+import {TooltipModule} from 'ngx-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule,
+    TooltipModule.forRoot()
   ],
   declarations: [MainComponent, NavigationComponent, ToolbarComponent],
 })
-export class MainModule { }
+export class MainModule {
+}
